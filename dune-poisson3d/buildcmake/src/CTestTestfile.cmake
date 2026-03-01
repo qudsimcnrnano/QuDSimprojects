f93b428d675ecf0546d61c3425f1c55299308215
qudsimcnrnano <qdsimcnrnano@gmail.com>
2026-03-01 13:51:43 +0530

    Add files via upload

---
+++ dune-poisson3d/buildcmake/src/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/athira/dune-projects/dune-p3d/src
+# Build directory: /home/athira/dune-projects/dune-p3d/build-cmake/src
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ dune-poisson3d/buildcmake/src/DependInfo.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dune-p3d.dir/dune-p3d.cc.o"
+  "CMakeFiles/dune-p3d.dir/dune-p3d.cc.o.d"
+  "dune-p3d"
+  "dune-p3d.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/dune-p3d.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
