f320a64b0dab4f52375856cd45bfb7476e3719ee
qudsimcnrnano <qdsimcnrnano@gmail.com>
2026-03-01 13:49:21 +0530

    Create CTestTestfile.cmake

---
+++ dune-poisson3d/buildcmake/dune/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/athira/dune-projects/dune-p3d/dune
+# Build directory: /home/athira/dune-projects/dune-p3d/build-cmake/dune
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("p3d")
