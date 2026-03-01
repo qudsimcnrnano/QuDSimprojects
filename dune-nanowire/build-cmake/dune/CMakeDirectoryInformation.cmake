ee2b0b305102a3720e3c7fda8f6b24c1bf377a8a
qudsimcnrnano <qdsimcnrnano@gmail.com>
2026-03-01 11:39:11 +0530

    Add files via upload

---
+++ dune-nanowire/build-cmake/dune/CMakeDirectoryInformation.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/athira/dune-projects/dune-nanowire/dune/nanowire
+# Build directory: /home/athira/dune-projects/dune-nanowire/build-cmake/dune/nanowire
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
