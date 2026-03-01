e2ad0ec412e767af063690465e7721418fa78c4f
qudsimcnrnano <qdsimcnrnano@gmail.com>
2026-03-01 11:20:54 +0530

    Add files via upload

---
+++ dune-graphene/build-cmmake/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /home/athira/dune-projects/dune-graphene1
+# Build directory: /home/athira/dune-projects/dune-graphene1/build-cmake
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("src")
+subdirs("dune")
+subdirs("doc")
+subdirs("cmake/modules")
