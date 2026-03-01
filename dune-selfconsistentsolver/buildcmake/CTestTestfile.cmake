df3fef47177ed2dbe4f2ff0101c8f53b32b76d1c
qudsimcnrnano <qdsimcnrnano@gmail.com>
2026-03-01 14:09:06 +0530

    Delete dune-selfconsistentsolver/buildcmake/CTestTestfile.cmake

--- dune-selfconsistentsolver/buildcmake/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/athira/dune-projects/dune-sc1/dune
-# Build directory: /home/athira/dune-projects/dune-sc1/build-cmake/dune
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("sc1")
