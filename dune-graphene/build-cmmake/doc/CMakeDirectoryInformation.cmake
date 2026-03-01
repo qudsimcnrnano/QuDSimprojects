b60e161fafe6322b00835c9835baa4b3da5f4a6b
qudsimcnrnano <qdsimcnrnano@gmail.com>
2026-03-01 11:19:02 +0530

    Add files via upload

---
+++ dune-graphene/build-cmmake/doc/CMakeDirectoryInformation.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/athira/dune-projects/dune-graphene1/doc/doxygen
+# Build directory: /home/athira/dune-projects/dune-graphene1/build-cmake/doc/doxygen
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ dune-graphene/build-cmmake/doc/progress.make
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/doxygen_dune-graphene1"
+  "Doxyfile"
+  "Doxyfile.in"
+  "html"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/doxygen_dune-graphene1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
