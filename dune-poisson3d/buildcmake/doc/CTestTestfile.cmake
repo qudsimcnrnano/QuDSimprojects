a59dcbbe2d953a68e2e34517c15c0cb64e1dfba7
qudsimcnrnano <qdsimcnrnano@gmail.com>
2026-03-01 13:46:38 +0530

    Add files via upload

---
+++ dune-poisson3d/buildcmake/doc/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/athira/dune-projects/dune-p3d/doc
+# Build directory: /home/athira/dune-projects/dune-p3d/build-cmake/doc
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("doxygen")
