a30ce3a98d40d07e6a7bee005e661e3024188cb4
qudsimcnrnano <qdsimcnrnano@gmail.com>
2026-03-01 14:03:54 +0530

    Create dune-sc1-config.cmake

---
+++ dune-selfconsistentsolver/buildcmake/cmake/pkg/dune-sc1-config.cmake
@@ -0,0 +1,58 @@
+if(NOT dune-sc1_FOUND)
+# Whether this module is installed or not
+set(dune-sc1_INSTALLED ON)
+
+# Settings specific to the module
+
+# Package initialization
+
+####### Expanded from @PACKAGE_INIT@ by configure_package_config_file() #######
+####### Any changes to this file will be overwritten by the next CMake run ####
+####### The input file was dune-sc1-config.cmake.in                            ########
+
+get_filename_component(PACKAGE_PREFIX_DIR "${CMAKE_CURRENT_LIST_DIR}/../../../" ABSOLUTE)
+
+macro(set_and_check _var _file)
+  set(${_var} "${_file}")
+  if(NOT EXISTS "${_file}")
+    message(FATAL_ERROR "File or directory ${_file} referenced by variable ${_var} does not exist !")
+  endif()
+endmacro()
+
+macro(check_required_components _NAME)
+  foreach(comp ${${_NAME}_FIND_COMPONENTS})
+    if(NOT ${_NAME}_${comp}_FOUND)
+      if(${_NAME}_FIND_REQUIRED_${comp})
+        set(${_NAME}_FOUND FALSE)
+      endif()
+    endif()
+  endforeach()
+endmacro()
+
+####################################################################################
+
+#report other information
+set_and_check(dune-sc1_PREFIX "${PACKAGE_PREFIX_DIR}")
+set_and_check(dune-sc1_INCLUDE_DIRS "${PACKAGE_PREFIX_DIR}/include")
+set(dune-sc1_CXX_FLAGS "-std=c++17 /opt/local/petsc-3.15.2/arch-linux-cxx-debug/lib/libpetsc.so.3.15 /opt/local/slepc-3.15.2/arch-linux-cxx-debug/lib/libslepc.so.3.15")
+set(dune-sc1_CXX_FLAGS_DEBUG "-g")
+set(dune-sc1_CXX_FLAGS_MINSIZEREL "-Os -DNDEBUG")
+set(dune-sc1_CXX_FLAGS_RELEASE "-O3 -DNDEBUG")
+set(dune-sc1_CXX_FLAGS_RELWITHDEBINFO "-O2 -g -DNDEBUG")
+set(dune-sc1_DEPENDS "dune-common;dune-geometry;dune-grid;dune-istl;dune-localfunctions;dune-alugrid;dune-grid-howto;dune-gmsh4")
+set(dune-sc1_SUGGESTS "")
+set(dune-sc1_MODULE_PATH "${PACKAGE_PREFIX_DIR}/share/dune/cmake/modules")
+set(dune-sc1_LIBRARIES "")
+set(dune-sc1_HASPYTHON 0)
+set(dune-sc1_PYTHONREQUIRES "")
+
+# Lines that are set by the CMake build system via the variable DUNE_CUSTOM_PKG_CONFIG_SECTION
+
+
+#import the target
+if(dune-sc1_LIBRARIES)
+  get_filename_component(_dir "${CMAKE_CURRENT_LIST_FILE}" PATH)
+  include("${_dir}/dune-sc1-targets.cmake")
+endif()
+
+endif()
