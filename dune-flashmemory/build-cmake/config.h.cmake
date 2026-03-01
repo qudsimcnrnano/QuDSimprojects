6acb86744edc9dc3ea6eeb815baee4eb04f57471
qudsimcnrnano <qdsimcnrnano@gmail.com>
2026-03-01 17:12:23 +0530

    Add files via upload

---
+++ dune-flashmemory/build-cmake/config.h.cmake
@@ -0,0 +1,45 @@
+/* begin dune-nanowire
+   put the definitions for config.h specific to
+   your project here. Everything above will be
+   overwritten
+*/
+
+/* begin private */
+/* Name of package */
+#define PACKAGE "@DUNE_MOD_NAME@"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "@DUNE_MAINTAINER@"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "@DUNE_MOD_NAME@"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "@DUNE_MOD_NAME@ @DUNE_MOD_VERSION@"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "@DUNE_MOD_NAME@"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL "@DUNE_MOD_URL@"
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "@DUNE_MOD_VERSION@"
+
+/* end private */
+
+/* Define to the version of dune-nanowire */
+#define DUNE_NANOWIRE_VERSION "@DUNE_NANOWIRE_VERSION@"
+
+/* Define to the major version of dune-nanowire */
+#define DUNE_NANOWIRE_VERSION_MAJOR @DUNE_NANOWIRE_VERSION_MAJOR@
+
+/* Define to the minor version of dune-nanowire */
+#define DUNE_NANOWIRE_VERSION_MINOR @DUNE_NANOWIRE_VERSION_MINOR@
+
+/* Define to the revision of dune-nanowire */
+#define DUNE_NANOWIRE_VERSION_REVISION @DUNE_NANOWIRE_VERSION_REVISION@
+
+/* end dune-nanowire
+   Everything below here will be overwritten
+*/
